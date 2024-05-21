431d73a96574ea04b8bfd8e53c91c9d1a3af0852
lai2-24 <lai2-24@itismeucci.com>
2024-05-21 14:36:23 +0200

    commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import SingUp from './SingUp';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function App() {
+
+  const [mostraSingUp, setMostraSingUp] = useState(false);
+  const [mostraLogin, setMostraLogin] = useState(false);
+  const [token, setToken] = useState();
+  const [username , setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [info, setInfo] = useState();
+
+
+
+  async function SingUp(){
+    setMostraLogin(false);
+    const response = await fetch("http://localhost:8080/signup",{
+            method:"POST",
+            body : JSON.stringify({username: username, 
+                                   password: password, 
+                                   email: email})
+          }
+  )};
+  const json = await response.json();
+
+  if(json.status == true){
+    setInfo("Utente Registrato");
+  }
+  else{
+    setInfo("Errore durante la Registrazione");
+  }
+
+  async function Login(){
+    setMostraSingUp(false);
+    const response = await fetch("http://localhost:8080/login",{
+            method:"POST",
+            body : JSON.stringify({username: username, 
+                                   password: password})
+          }
+  )};
+
+  return{
+    
+  };
+
 }
 
 export default App;
